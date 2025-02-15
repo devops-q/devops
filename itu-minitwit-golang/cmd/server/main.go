5af1c70a01ff81310227470dcbf5133029b42f1b
Sir Eraisuithon <mj@lpj-software.dk>
2025-02-15 22:49:36 +0100

    Setup initialization of database, including creating db-file if it is missing

--- itu-minitwit-golang/cmd/server/main.go
+++ itu-minitwit-golang/cmd/server/main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"itu-minitwit/config"
 	"itu-minitwit/internal/api"
+	"itu-minitwit/internal/models"
 	"log"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
@@ -13,6 +19,8 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to load configuration: %v", err)
 	}
+
+	initDb(cfg)
 
 	if cfg.Environment == "production" {
 		gin.SetMode(gin.ReleaseMode)
@@ -23,5 +31,23 @@ func main() {
 	api.SetupRoutes(r, cfg)
 
 	log.Printf("Server starting on port %d", cfg.Port)
-	r.Run(fmt.Sprintf(":%d", cfg.Port))
+	r.Run(fmt.Sprintf("localhost:%d", cfg.Port))
+}
+
+func initDb(cfg *config.Config) {
+	if _, err := os.Stat(cfg.DBPath); err != nil {
+		fmt.Print("Creating new db file")
+		path := strings.Split(cfg.DBPath, "/")
+		if len(path) > 0 {
+			dirPath := strings.Join(path[:len(path)-1], "/")
+			os.MkdirAll(dirPath, 0755)
+		}
+		os.Create(cfg.DBPath)
+	}
+
+	db, dbErr := gorm.Open(sqlite.Open(cfg.DBPath), &gorm.Config{})
+	if dbErr != nil {
+		log.Fatalf("Could not connect to database: %v", dbErr)
+	}
+	db.AutoMigrate(&models.User{}, &models.Message{})
 }
