ec6f5a2f61a5563d438815f29ef4c7c14ad8478c
Sir Eraisuithon <mj@lpj-software.dk>
2025-02-16 00:38:33 +0100

    Moved initDB to seperate file

--- itu-minitwit-golang/cmd/server/main.go
+++ itu-minitwit-golang/cmd/server/main.go
@@ -10,8 +10,6 @@ import (
 	"strings"
 
 	"github.com/gin-gonic/gin"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
 )
 
 func main() {
@@ -20,7 +18,7 @@ func main() {
 		log.Fatalf("Failed to load configuration: %v", err)
 	}
 
-	initDb(cfg)
+	database.InitDb(cfg)
 
 	if cfg.Environment == "production" {
 		gin.SetMode(gin.ReleaseMode)
@@ -32,22 +30,4 @@ func main() {
 
 	log.Printf("Server starting on port %d", cfg.Port)
 	r.Run(fmt.Sprintf("localhost:%d", cfg.Port))
-}
-
-func initDb(cfg *config.Config) {
-	if _, err := os.Stat(cfg.DBPath); err != nil {
-		fmt.Print("Creating new db file")
-		path := strings.Split(cfg.DBPath, "/")
-		if len(path) > 0 {
-			dirPath := strings.Join(path[:len(path)-1], "/")
-			os.MkdirAll(dirPath, 0755)
-		}
-		os.Create(cfg.DBPath)
-	}
-
-	db, dbErr := gorm.Open(sqlite.Open(cfg.DBPath), &gorm.Config{})
-	if dbErr != nil {
-		log.Fatalf("Could not connect to database: %v", dbErr)
-	}
-	db.AutoMigrate(&models.User{}, &models.Message{})
 }
