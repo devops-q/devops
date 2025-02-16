94d3a17e1bd007e9b6eda7d657547c6214c7944e
Sir Eraisuithon <mj@lpj-software.dk>
2025-02-16 01:04:34 +0100

    Added another endpoint to show how to get nested data on the structs

--- itu-minitwit-golang/internal/api/handlers/database_example_handlers.go
+++ itu-minitwit-golang/internal/api/handlers/database_example_handlers.go
@@ -15,7 +15,7 @@ func GetUsersHandler(c *gin.Context) {
 	// Finds all that matches the given array and fills it
 	db.Find(&users)
 
-	c.JSON(http.StatusOK, gin.H{"data": users})
+	c.JSON(http.StatusOK, gin.H{"users": users})
 }
 
 func CreateUserHandler(c *gin.Context) {
@@ -50,7 +50,7 @@ func FindUserWithName(c *gin.Context) {
 
 	// If we want to search for zero/falsy values do like this
 	// (specifying flagged is false in struct is not needed, as it is default)
-	db.Where(&models.Message{Flagged: false}, "Flagged")
+	// db.Where(&models.Message{Flagged: false}, "Flagged")
 
 	// To check if something was found, we need to look at the PK
 	// as that will be populated if something was found
@@ -59,4 +59,14 @@ func FindUserWithName(c *gin.Context) {
 	} else {
 		c.JSON(http.StatusOK, user)
 	}
+}
+
+func GetAllUsersWithNonFlaggedMessages(c *gin.Context) {
+	db := c.MustGet("DB").(*gorm.DB)
+
+	var users []models.User
+	// By default nested objects will not be inclueded
+	// to include them, preload them on the query
+	db.Preload("Messages", "flagged=0").Find(&users)
+	c.JSON(http.StatusOK, users)
 }
--- itu-minitwit-golang/internal/api/routes.go
+++ itu-minitwit-golang/internal/api/routes.go
@@ -14,9 +14,11 @@ func SetupRoutes(r *gin.Engine, cfg *config.Config) {
 	r.GET("/ping", handlers.PingHandler)
 	r.GET("/hello/:name", handlers.HelloHandler)
 
+	// Examples of how to use the ORM in endpoints
 	r.GET("/users", handlers.GetUsersHandler)
 	r.GET("/user/create/:name", handlers.CreateUserHandler)
 	r.GET("/user/find/:name", handlers.FindUserWithName)
+	r.GET("/users/messages", handlers.GetAllUsersWithNonFlaggedMessages)
 
 	// Add more routes here
 }
