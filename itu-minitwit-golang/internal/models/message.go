927ee16932162fb162c5e3c603ff45659f56cc33
Sir Eraisuithon <mj@lpj-software.dk>
2025-02-16 01:04:03 +0100

    Made author a pointed so it is null when uninitialized, instead of the "default" User

--- itu-minitwit-golang/internal/models/message.go
+++ itu-minitwit-golang/internal/models/message.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type Message struct {
 	gorm.Model
 	AuthorID uint   `gorm:"not null"`
-	Author   User   `gorm:"foreignkey:author_id;association_foreignkey:id"`
+	Author   *User  `gorm:"foreignkey:author_id;association_foreignkey:id"`
 	Text     string `gorm:"not null"`
 	Flagged  bool   `gorm:"default:false"`
 }
