704444c4710712d53af0b5e9af952845be3ed37b
Sir Eraisuithon <mj@lpj-software.dk>
2025-02-17 23:11:38 +0100

    Relevant comments in code

--- itu-minitwit-golang/setup/test_setup.go
+++ itu-minitwit-golang/setup/test_setup.go
@@ -12,6 +12,9 @@ import (
 )
 
 func SetupTest() *gin.Engine {
+	// Wierd hack to get working directory to project root
+	// Needed as running tests changes the cwd to the directory
+	// the test-file is located in
 	_, filename, _, _ := runtime.Caller(0)
 	dir := path.Join(path.Dir(filename), "..")
 	err := os.Chdir(dir)
@@ -19,6 +22,7 @@ func SetupTest() *gin.Engine {
 		panic(err)
 	}
 
+	// Load config, setup db, and setup router
 	cfg, err := config.LoadConfig(true)
 	if err != nil {
 		fmt.Printf("Could not load test config, %v", err)
