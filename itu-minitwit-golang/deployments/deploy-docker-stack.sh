1e14d6d0f3c39c2588cdd590ac85bdb3ce43c118
Alin Plamadeala <alpl@itu.dk>
2025-02-17 16:19:06 +0100

    README.md

--- itu-minitwit-golang/deployments/deploy-docker-stack.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-HOST=$1
-
-docker context create digitalocean --docker "host=ssh://root@152.42.150.137"
-
-docker swarm init
-
-docker stack deploy -c docker-compose.yml itu-minitwit
