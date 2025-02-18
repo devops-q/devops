5c9b70581319f644c63f29ff46bf1cf784c97964
Alin Plamadeala <alpl@itu.dk>
2025-02-18 16:02:50 +0100

    move public to timeline

--- itu-minitwit-golang/internal/api/handlers/timeline_handler.go
+++ itu-minitwit-golang/internal/api/handlers/timeline_handler.go
@@ -9,6 +9,35 @@ import (
 	"net/http"
 )
 
+func PublicTimelineHandler(c *gin.Context) {
+	db := c.MustGet("DB").(*gorm.DB)
+	cfg := c.MustGet("Config").(*config.Config)
+
+	value, userLoggedIn := c.Get("user")
+	var user *models.User
+
+	if userLoggedIn {
+		user = value.(*models.User)
+	} else {
+		user = nil
+	}
+
+	var messages []models.Message
+	db.Model(&models.Message{}).
+		Preload("Author").
+		Where(map[string]interface{}{"flagged": false}).
+		Order("created_at desc").Limit(cfg.PerPage).
+		Find(&messages)
+
+	c.HTML(http.StatusOK, "layout.html", gin.H{
+		"Title":    "Public Timeline",
+		"Endpoint": "/public",
+		"body":     "timeline",
+		"User":     user,
+		"Messages": messages,
+	})
+}
+
 func TimelineHandler(c *gin.Context) {
 	log.Printf("We got a visitor from: %s\n", c.ClientIP())
 
