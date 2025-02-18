de1273ae8b63707131489dc53bbbb590ad3a95c1
Alin Plamadeala <alpl@itu.dk>
2025-02-18 15:10:35 +0100

    endpoint /

--- itu-minitwit-golang/internal/api/handlers/timeline_handler.go
+++ itu-minitwit-golang/internal/api/handlers/timeline_handler.go
@@ -1,23 +1,43 @@
 package handlers
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"itu-minitwit/config"
+	"itu-minitwit/internal/models"
+	"log"
+	"net/http"
 )
 
 func TimelineHandler(c *gin.Context) {
-    c.HTML(http.StatusOK, "layout.html", gin.H{
-        "Title":   "Sign In",
-        "body":    "timeline",
-        "Error":   "",
-        "Username": "",
-		"UserID" : gin.H{
-			"Username" : "phbl",
-			"UserID": 3,
-		},
-        "Email":   "",
-		"Endpoint" : "/",
-    })
-}
+	log.Printf("We got a visitor from: %s\n", c.ClientIP())
 
+	db := c.MustGet("DB").(*gorm.DB)
+	cfg := c.MustGet("Config").(*config.Config)
+
+	value, userLoggedIn := c.Get("user")
+	var user *models.User
+
+	if !userLoggedIn {
+		c.Redirect(http.StatusFound, "/public")
+	}
+
+	user = value.(*models.User)
+
+	var messages []models.Message
+	db.
+		Model(&models.Message{}).Preload("Author").
+		Joins("JOIN users ON messages.author_id = users.id").
+		Where("messages.flagged = ? AND (messages.author_id = ? OR messages.author_id IN (SELECT following_id FROM follower WHERE user_id = ?))",
+			false, user.ID, user.ID).
+		Order("messages.created_at desc").Limit(cfg.PerPage).
+		Find(&messages)
+
+	c.HTML(http.StatusOK, "layout.html", gin.H{
+		"Title":    "My Timeline",
+		"body":     "timeline",
+		"User":     user,
+		"Messages": messages,
+		"Endpoint": "/",
+	})
+}
