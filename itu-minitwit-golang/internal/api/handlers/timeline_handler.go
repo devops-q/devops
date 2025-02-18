8b2224a60692c404b1c6075be3bdca84ef5739b5
Alin Plamadeala <alpl@itu.dk>
2025-02-18 15:10:35 +0100

    endpoint /

--- itu-minitwit-golang/internal/api/handlers/timeline_handler.go
+++ itu-minitwit-golang/internal/api/handlers/timeline_handler.go
@@ -1,36 +1,43 @@
 package handlers
 
 import (
-	"itu-minitwit/internal/models"
-	"itu-minitwit/internal/utils"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"itu-minitwit/config"
+	"itu-minitwit/internal/models"
+	"log"
+	"net/http"
 )
 
 func TimelineHandler(c *gin.Context) {
+	log.Printf("We got a visitor from: %s\n", c.ClientIP())
 
-	
-	var user *models.User = utils.GetUserFomContext(c)
+	db := c.MustGet("DB").(*gorm.DB)
+	cfg := c.MustGet("Config").(*config.Config)
 
-	if user != nil {
-    c.HTML(http.StatusOK, "layout.html", gin.H{
-        "Title":   "Sign In",
-        "body":    "timeline",
-        "Error":   "",
-        "Username": user.Username,
-		"UserID" : user.ID,
-        "Email":   user.Email,
-		"Endpoint" : c.FullPath(),
-    })
-} else {
-	// This is when the user is not logged in. 
+	value, userLoggedIn := c.Get("user")
+	var user *models.User
+
+	if !userLoggedIn {
+		c.Redirect(http.StatusFound, "/public")
+	}
+
+	user = value.(*models.User)
+
+	var messages []models.Message
+	db.
+		Model(&models.Message{}).Preload("Author").
+		Joins("JOIN users ON messages.author_id = users.id").
+		Where("messages.flagged = ? AND (messages.author_id = ? OR messages.author_id IN (SELECT following_id FROM follower WHERE user_id = ?))",
+			false, user.ID, user.ID).
+		Order("messages.created_at desc").Limit(cfg.PerPage).
+		Find(&messages)
+
 	c.HTML(http.StatusOK, "layout.html", gin.H{
-        "Title":   "Sign In",
-        "body":    "timeline",
-        "Error":   "",
-		"Endpoint" : c.FullPath(),
-    })
+		"Title":    "My Timeline",
+		"body":     "timeline",
+		"User":     user,
+		"Messages": messages,
+		"Endpoint": "/",
+	})
 }
-}
-
