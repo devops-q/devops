4c1a772ef0a506f9e4dabf8b63242dd6799f77d1
Alin Plamadeala <alpl@itu.dk>
2025-02-18 22:32:23 +0100

    implement tests

--- itu-minitwit-golang/tests/app_test.go
+++ itu-minitwit-golang/tests/app_test.go
@@ -1,16 +1,28 @@
 package tests
 
 import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"io"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
+	"os/exec"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
-const baseURL = "http://localhost:8181"
+const host = "http://127.0.0.1"
+const port = "8181"
+const baseURL = host + ":" + port
+
+type MinitwitTestSuite struct {
+	suite.Suite
+	Cmd exec.Cmd
+}
+
+func TestMinitwitTestSuite(t *testing.T) {
+	suite.Run(t, new(MinitwitTestSuite))
+}
 
 // helper functions
 func register(t *testing.T, username, password, password2, email string) *http.Response {
@@ -76,120 +88,120 @@ func addMessage(t *testing.T, client *http.Client, text string) *http.Response {
 	return resp
 }
 
-func TestRegister(t *testing.T) {
-	resp := register(t, "user01", "default", "", "")
+func (suite *MinitwitTestSuite) TestRegister() {
+	resp := register(suite.T(), "user01", "default", "", "")
 	body, _ := io.ReadAll(resp.Body)
-	assert.Contains(t, string(body), "You were successfully registered and can login now")
+	assert.Contains(suite.T(), string(body), "You were successfully registered and can login now")
 
-	resp = register(t, "user01", "default", "", "")
+	resp = register(suite.T(), "user01", "default", "", "")
 	body, _ = io.ReadAll(resp.Body)
-	assert.Contains(t, string(body), "The username is already taken")
+	assert.Contains(suite.T(), string(body), "The username is already taken")
 
-	resp = register(t, "", "default", "", "")
+	resp = register(suite.T(), "", "default", "", "")
 	body, _ = io.ReadAll(resp.Body)
-	assert.Contains(t, string(body), "You have to enter a username")
+	assert.Contains(suite.T(), string(body), "You have to enter a username")
 
-	resp = register(t, "meh", "", "", "")
+	resp = register(suite.T(), "meh", "", "", "")
 	body, _ = io.ReadAll(resp.Body)
-	assert.Contains(t, string(body), "You have to enter a password")
+	assert.Contains(suite.T(), string(body), "You have to enter a password")
 
-	resp = register(t, "meh", "x", "y", "")
+	resp = register(suite.T(), "meh", "x", "y", "")
 	body, _ = io.ReadAll(resp.Body)
-	assert.Contains(t, string(body), "The two passwords do not match")
+	assert.Contains(suite.T(), string(body), "The two passwords do not match")
 
-	resp = register(t, "meh", "foo", "", "broken")
+	resp = register(suite.T(), "meh", "foo", "", "broken")
 	body, _ = io.ReadAll(resp.Body)
-	assert.Contains(t, string(body), "You have to enter a valid email address")
+	assert.Contains(suite.T(), string(body), "You have to enter a valid email address")
 }
 
-func TestLoginLogout(t *testing.T) {
-	resp, client := registerAndLogin(t, "user1", "default")
+func (suite *MinitwitTestSuite) TestLoginLogout() {
+	resp, client := registerAndLogin(suite.T(), "user1", "default")
 	body, _ := io.ReadAll(resp.Body)
-	assert.Contains(t, string(body), "You were logged in")
+	assert.Contains(suite.T(), string(body), "You were logged in")
 
-	resp = logout(t, client)
+	resp = logout(suite.T(), client)
 	body, _ = io.ReadAll(resp.Body)
-	assert.Contains(t, string(body), "You were logged out")
+	assert.Contains(suite.T(), string(body), "You were logged out")
 
-	resp, client = login(t, "user1", "wrongpassword")
+	resp, client = login(suite.T(), "user1", "wrongpassword")
 	body, _ = io.ReadAll(resp.Body)
-	assert.Contains(t, string(body), "Invalid password")
+	assert.Contains(suite.T(), string(body), "Invalid password")
 
-	resp, client = login(t, "user2", "wrongpassword")
+	resp, client = login(suite.T(), "user2", "wrongpassword")
 	body, _ = io.ReadAll(resp.Body)
-	assert.Contains(t, string(body), "Invalid username")
+	assert.Contains(suite.T(), string(body), "Invalid username")
 }
 
-func TestMessageRecording(t *testing.T) {
-	_, client := registerAndLogin(t, "foo", "default")
-	addMessage(t, client, "test message 1")
-	addMessage(t, client, "<test message 2>")
+func (suite *MinitwitTestSuite) TestMessageRecording() {
+	_, client := registerAndLogin(suite.T(), "foo", "default")
+	addMessage(suite.T(), client, "test message 1")
+	addMessage(suite.T(), client, "<test message 2>")
 
 	resp, err := http.Get(baseURL + "/")
-	assert.NoError(t, err)
+	assert.NoError(suite.T(), err)
 	body, _ := io.ReadAll(resp.Body)
 	bodyStr := string(body)
-	assert.Contains(t, bodyStr, "test message 1")
-	assert.Contains(t, bodyStr, "&lt;test message 2&gt;")
+	assert.Contains(suite.T(), bodyStr, "test message 1")
+	assert.Contains(suite.T(), bodyStr, "&lt;test message 2&gt;")
 }
 
-func TestTimelines(t *testing.T) {
-	_, client1 := registerAndLogin(t, "foo", "default")
-	addMessage(t, client1, "the message by foo")
-	logout(t, client1)
+func (suite *MinitwitTestSuite) TestTimelines() {
+	_, client1 := registerAndLogin(suite.T(), "foo", "default")
+	addMessage(suite.T(), client1, "the message by foo")
+	logout(suite.T(), client1)
 
-	_, client2 := registerAndLogin(t, "bar", "default")
-	addMessage(t, client2, "the message by bar")
+	_, client2 := registerAndLogin(suite.T(), "bar", "default")
+	addMessage(suite.T(), client2, "the message by bar")
 
 	resp, err := client2.Get(baseURL + "/public")
-	assert.NoError(t, err)
+	assert.NoError(suite.T(), err)
 	body, _ := io.ReadAll(resp.Body)
 	bodyStr := string(body)
-	assert.Contains(t, bodyStr, "the message by foo")
-	assert.Contains(t, bodyStr, "the message by bar")
+	assert.Contains(suite.T(), bodyStr, "the message by foo")
+	assert.Contains(suite.T(), bodyStr, "the message by bar")
 
 	resp, err = client2.Get(baseURL + "/")
-	assert.NoError(t, err)
+	assert.NoError(suite.T(), err)
 	body, _ = io.ReadAll(resp.Body)
 	bodyStr = string(body)
-	assert.NotContains(t, bodyStr, "the message by foo")
-	assert.Contains(t, bodyStr, "the message by bar")
+	assert.NotContains(suite.T(), bodyStr, "the message by foo")
+	assert.Contains(suite.T(), bodyStr, "the message by bar")
 
 	resp, err = client2.Get(baseURL + "/foo/follow")
-	assert.NoError(t, err)
+	assert.NoError(suite.T(), err)
 	body, _ = io.ReadAll(resp.Body)
-	assert.Contains(t, string(body), "You are now following &#34;foo&#34;")
+	assert.Contains(suite.T(), string(body), "You are now following &#34;foo&#34;")
 
 	resp, err = client2.Get(baseURL + "/")
-	assert.NoError(t, err)
+	assert.NoError(suite.T(), err)
 	body, _ = io.ReadAll(resp.Body)
 	bodyStr = string(body)
-	assert.Contains(t, bodyStr, "the message by foo")
-	assert.Contains(t, bodyStr, "the message by bar")
+	assert.Contains(suite.T(), bodyStr, "the message by foo")
+	assert.Contains(suite.T(), bodyStr, "the message by bar")
 
 	resp, err = client2.Get(baseURL + "/bar")
-	assert.NoError(t, err)
+	assert.NoError(suite.T(), err)
 	body, _ = io.ReadAll(resp.Body)
 	bodyStr = string(body)
-	assert.NotContains(t, bodyStr, "the message by foo")
-	assert.Contains(t, bodyStr, "the message by bar")
+	assert.NotContains(suite.T(), bodyStr, "the message by foo")
+	assert.Contains(suite.T(), bodyStr, "the message by bar")
 
 	resp, err = client2.Get(baseURL + "/foo")
-	assert.NoError(t, err)
+	assert.NoError(suite.T(), err)
 	body, _ = io.ReadAll(resp.Body)
 	bodyStr = string(body)
-	assert.Contains(t, bodyStr, "the message by foo")
-	assert.NotContains(t, bodyStr, "the message by bar")
+	assert.Contains(suite.T(), bodyStr, "the message by foo")
+	assert.NotContains(suite.T(), bodyStr, "the message by bar")
 
 	resp, err = client2.Get(baseURL + "/foo/unfollow")
-	assert.NoError(t, err)
+	assert.NoError(suite.T(), err)
 	body, _ = io.ReadAll(resp.Body)
-	assert.Contains(t, string(body), "You are no longer following &#34;foo&#34;")
+	assert.Contains(suite.T(), string(body), "You are no longer following &#34;foo&#34;")
 
 	resp, err = client2.Get(baseURL + "/")
-	assert.NoError(t, err)
+	assert.NoError(suite.T(), err)
 	body, _ = io.ReadAll(resp.Body)
 	bodyStr = string(body)
-	assert.NotContains(t, bodyStr, "the message by foo")
-	assert.Contains(t, bodyStr, "the message by bar")
+	assert.NotContains(suite.T(), bodyStr, "the message by foo")
+	assert.Contains(suite.T(), bodyStr, "the message by bar")
 }
