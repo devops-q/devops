a7b99e5615720c1726a14ad14e06ee904e07949a
Alin Plamadeala <alpl@itu.dk>
2025-02-19 00:02:48 +0100

    GetMessagesByAuthor

---
+++ itu-minitwit-golang/internal/service/message_service.go
@@ -0,0 +1,22 @@
+package service
+
+import (
+	"gorm.io/gorm"
+	"itu-minitwit/internal/models"
+)
+
+func GetMessagesByAuthor(db *gorm.DB, userID uint, perPage int) ([]models.Message, error) {
+	var messages []models.Message
+	err := db.Model(&models.Message{}).
+		Preload("Author").
+		Where("author_id = ? AND flagged = ?", userID, false).
+		Order("created_at desc").
+		Limit(perPage).
+		Find(&messages).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return messages, nil
+}
