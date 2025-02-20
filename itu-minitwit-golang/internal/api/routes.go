7af61c808a85ec5c8b98c0a460705c4371d9903f
Sir Eraisuithon <mj@lpj-software.dk>
2025-02-20 13:14:43 +0100

    Removed duplicate handler

--- itu-minitwit-golang/internal/api/routes.go
+++ itu-minitwit-golang/internal/api/routes.go
@@ -36,8 +36,6 @@ func SetupRoutes(r *gin.Engine, cfg *config.Config) {
 	r.GET("/user/current", handlers.GetUserInSession)
 	r.GET("/user/force-login/:id", handlers.ForceSetUserId)
 	r.GET("/latest", handlers.GetLatest)
-	
-	r.POST("/add_message", handlers.MessageHandler)
 
 	r.POST("/add_message", handlers.MessageHandler)
 
