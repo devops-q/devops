4c0360409df152707d53aa86c6a322e67daa0703
Alin Plamadeala <alpl@itu.dk>
2025-02-24 14:34:39 +0100

    update CI workflow to use golangci-lint and fix test script name

--- itu-minitwit-golang/run_tests.sh
+++ itu-minitwit-golang/run_tests.sh
@@ -9,6 +9,8 @@ fi
 # Remove test database if exists
 rm -f database.test.sqlite
 
+DB_PATH=./database.test.sqlite go run cmd/create_api_user/main.go -username=test -password=test
+
 # Start the server in background
 PORT=8181 DB_PATH=./database.test.sqlite go run cmd/server/main.go > /dev/null 2>&1 &
 
@@ -17,9 +19,13 @@ sleep 2
 
 # Run tests
 go test ./tests
+TEST_EXIT_CODE=$?
+
 
 # Kill any existing server process on port 8181
 kill "$(lsof -t -i:8181)"
 
 # Clean up test database
 rm -f database.test.sqlite
+
+exit $TEST_EXIT_CODE
