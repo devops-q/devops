fbbe741e5066f8d86d195bcd70632d2851e7fd61
Philip Boesen Blomholt <phbl@itu.dk>
2025-03-23 15:24:40 +0100

    fix mount

--- itu-minitwit-golang/deployments/terraform/infrastructure/files/init_script.sh
+++ itu-minitwit-golang/deployments/terraform/infrastructure/files/init_script.sh
@@ -13,6 +13,8 @@ mkdir -p /root/data
 # Create and populate the
 mkdir -p /root/prometheus
 
+mkdir -p mnt/mount/data
+
 cat <<'EOF' > /root/prometheus/prometheus.yml
 global:
   scrape_interval: 15s  # By default, scrape targets every 15 seconds.
