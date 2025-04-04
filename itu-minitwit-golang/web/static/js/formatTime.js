4bd3bd7c068516fc0b39b47d842baf48cb8d1187
Sir Eraisuithon <8248789+SirDancealot@users.noreply.github.com>
2025-04-04 11:45:18 +0200

    263 group o review (#310)
    * Added javascript to format time to local time
    
    * made it actually call formatTimeLocal (hopefully)
    
    * Re added thing removed by merge
    
    * moved js to the correct location
    
    * Maybe finally fixed formatting
    
    * Changed load event to "DOMContentLoaded" event

--- itu-minitwit-golang/web/static/js/formatTime.js
+++ itu-minitwit-golang/web/static/js/formatTime.js
@@ -1,5 +1,5 @@
-window.addEventListener("load", (event) => formatTimeLocal)
-function formatTimeLocal() {
+window.addEventListener('DOMContentLoaded', init, false);
+function formatTimeLocal(this, event) {
     var times = document.getElementsByClassName("time")
     for (let element of times) {
         var datetime = new Date(element.innerHTML)
